40519cc3819dfda0bd2db45ec97746b5445f810b
PEDRO  PRATES <157413476+PedroPrates08@users.noreply.github.com>
2024-01-22 16:16:16 -0300

    Add files via upload

---
+++ a.py
@@ -0,0 +1,15 @@
+#Escreva um programa que calcule a média de três notas.
+
+#escrevendo as notas
+nota1 = float(input("Escreva nota: "))
+nota2 = float(input("Escreva nota: "))
+nota3 = float(input("Escreva nota: "))
+
+#somando os numeors adicionados 
+soma = nota1 + nota2 + nota3
+#resultado da soma dividido por 2
+resultado = soma /2
+
+print(resultado)
+
+
